b90b49bb5d9b80572519ae1e1e86ae27718cd4e1
Mengxi Zhang <zmx94824@gmail.com>
2020-04-11 17:00:15 -0400

    Java NIO copy file uses channel transfer

--- java_nio/copy_file_channel/CopyFile.java
+++ java_nio/copy_file_channel/CopyFile.java
@@ -3,7 +3,7 @@ package java_nio.copy_file_channel;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
+// import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 class CopyFile {
@@ -17,17 +17,18 @@ class CopyFile {
         FileChannel inputChannel = fis.getChannel();
         FileChannel outputChannel = fos.getChannel();
 
-        ByteBuffer buf = ByteBuffer.allocate(64);
-        while (true) {
-            int read = inputChannel.read(buf);
+        // ByteBuffer buf = ByteBuffer.allocate(64);
+        // while (true) {
+        //     int read = inputChannel.read(buf);
 
-            if (read == -1) break;
+        //     if (read == -1) break;
 
-            buf.flip();
+        //     buf.flip();
 
-            outputChannel.write(buf);
-            buf.clear();
-        }
+        //     outputChannel.write(buf);
+        //     buf.clear();
+        // }
+        outputChannel.transferFrom(inputChannel, 0, 64);
 
         fis.close();
         fos.close();
