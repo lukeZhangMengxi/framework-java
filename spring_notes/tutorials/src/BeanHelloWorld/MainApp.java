58f470421606e3b1d06449910285662de95602d0
Mengxi Zhang <zmx94824@gmail.com>
2020-10-25 00:22:53 -0700

    note for IoC Spring container

--- spring_notes/tutorials/src/BeanHelloWorld/MainApp.java
+++ spring_notes/tutorials/src/BeanHelloWorld/MainApp.java
@@ -7,7 +7,7 @@ public class MainApp {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("BeanHelloWorld/Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld obj = (HelloWorld) context.getBean("an-id", HelloWorld.class);
         obj.getMessage();
     }
 }
