5aa6cfd6c98b34506083844f15e1b4cb8267902b
Mengxi Zhang <zmx94824@gmail.com>
2020-10-25 16:38:51 -0700

    update beanHelloWorld

--- spring_notes/tutorials/src/BeanHelloWorld/MainApp.java
+++ spring_notes/tutorials/src/BeanHelloWorld/MainApp.java
@@ -7,7 +7,13 @@ public class MainApp {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("BeanHelloWorld/Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("an-id", HelloWorld.class);
-        obj.getMessage();
+        HelloWorld obj1 = (HelloWorld) context.getBean("an-id", HelloWorld.class);
+        HelloWorld obj2 = (HelloWorld) context.getBean("an-id", HelloWorld.class);
+        if (obj1 == obj2) {
+            System.out.println("Same Bean ID get the SAME object");
+        } else {
+            System.out.println("Same Bean ID get DIFFERENT objects");
+        }
+        obj1.getMessage();
     }
 }
